a727a4eb34afe303f9c38c0836df3b6aa698ca70
RuneSong-ctrl <148033431+RuneSong-ctrl@users.noreply.github.com>
2024-02-25 00:29:55 +0800

    Add files via upload

---
+++ final_project/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
+
+let users = [];
+
+// Function to check if the user exists
+const doesExist = (username) => {
+  let userswithsamename = users.filter((user) => {
+    return user.username === username;
+  });
+  return userswithsamename.length > 0;
+};
+
+// Function to check if the user is authenticated
+const authenticatedUser = (username, password) => {
+  let validusers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  return validusers.length > 0;
+};
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Middleware function for authentication
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, "access", (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.use("/customer/auth/*", authenticateToken);
+
+const PORT = 5000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT, () => console.log("Server is running"));
